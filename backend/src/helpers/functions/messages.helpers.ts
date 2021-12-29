4d4feb49321fc9372767de4346ce5dee3dec42c7
Enoch <enoch@getinnotized.com>
2021-12-29 14:01:51 +0000

    excludes token from url for sms signup process

--- backend/src/helpers/functions/messages.helpers.ts
+++ backend/src/helpers/functions/messages.helpers.ts
@@ -2,7 +2,9 @@ import config from 'config';
 const callbackurl = <string>config.get('CALLBACK_URL') || '';
 
 const accountCreationMsg = (firstName: string, token: string, pin: string) => {
-    const str = `Welcome ${firstName}. Kindly follow this link ${callbackurl}/auth/activate-account?token=${token} to activate your account.
+    // const str = `Welcome ${firstName}. Kindly follow this link ${callbackurl}/auth/activate-account?token=${token} to activate your account.
+    // Your PIN is ${pin}`;
+    const str = `Welcome ${firstName}. Kindly follow this link ${callbackurl}/auth/activate-account?token=null to activate your account.
     Your PIN is ${pin}`;
 
     return str;
